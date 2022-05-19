e164ea8caea6e19672e2964c63368d6844d2c43f
Mugilshark <94998254+Mugilshark@users.noreply.github.com>
2022-05-19 17:46:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+var first_name = "Mugil";
+var last_name = " Neethi Devan";
+
+console.log(first_name);
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var replace_name = full_name.replace("Mugil", "Nithila");
+console.log(replace_name);
+
+var making_lower_case = full_name.toLowerCase();
+console.log(making_lower_case);
+
+var making_upper_case = full_name.toUpperCase();
+console.log(making_upper_case);
+
+var a = 6;
+var b = 4;
+console.log(a);
+console.log(b);
+
+var add = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub);
+
+var multi = a * b;
+console.log(multi);
+
+var div = a / b;
+console.log(div);
+
+var square_root = Math.sqrt(b);
+console.log(square_root);
+
+var maximum_number = Math.max(a, b);
+console.log(maximum_number);
+
+var minimum_number = Math.min(a, b);
+console.log(minimum_number);
+
+var power = Math.pow(a, b);
+console.log(power);
